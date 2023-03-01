910e90cff2edd9ba447acea1589c6d9251d4e9ec
Shade-tm <theo.ebner@edu.htl-villach.at>
2023-03-01 19:36:43 +0100

    first few lines, research for the data and print it

--- web_scraper.py
+++ web_scraper.py
@@ -1,17 +1,14 @@
 import pandas as pd
 import requests
-from bs4 import BeautifulSoup
+# python -m pipreqs.pipreqs . --force in current file for filling requirements.txt
 
-url = "https://sports.tipico.de/de"
+# URL location -> Developer Tools -> Network -> try and error until response is correct 
+url = "https://sports.tipico.de/json/program/hourEvents/2?oneSectionResult=true&language=de&isLoggedIn=0&licenseRegion=DE&maxMarkets=5"
+
+# response object of upcoming events. important things: events, matchoddgroups
+# events object contains list of id's. each id is a footbal match with the name of both teams, etc...
+# matchoddgroups object contains a list of id's (identical of what are in events). each id is the footbal match with all betting options 
 
 headers = {"User-Agent":"Mozilla/5.0"}
-
-response = requests.get(url, headers=headers)
-
-soup = BeautifulSoup(response.content, "html.parser")
-
-title = soup.title
-print(title)
-
-app = soup.find(id = "strong")
-print(app)
+upcoming_events = requests.get(url, headers=headers) # full object of upcoming events, headers simulate a real browser not a programm without headers status_code=403
+print(upcoming_events.json()) 
