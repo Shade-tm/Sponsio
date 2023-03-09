f69ae96c25f60e597d1e426827f5ec00949a17ed
Shade-tm <ebnert@edu.htl-villach.at>
2023-03-09 23:33:35 +0100

    rename function parse_df to list_of_best_odds and fully implement that function

--- event.py
+++ event.py
@@ -1,40 +1,36 @@
 import pandas as pd
 import numpy as np
 from api_calling import OddsApiCaller
-import json
 
 class Event: 
     def __init__(self):
-        self.data = pd.read_json('odds.json') # TODO: should be a paramater and the __init__ should receive it from the main 
+        self.data = pd.read_json('odds.json') # TODO: should be a paramater and the __init__ should receive the data from the main-function 
 
-    def parse_df(self):
-        event_outcomes = []
+    def list_of_best_odds(self):
+        all_best_odds = []
         events = self.data['bookmakers']
-        # print(events)
-        # print(events[3])
         for event in events:
-            # print(f"{len(event)} \n\n\n")
-            # print(event[2]['markets'][0]['outcomes']
             for count_bookmaker in range(len(event)):
-                # print(count_bookmaker)
-                # print(f"{event[count_bookmaker]['title']}\n")
                 num_outcomes = len(event[count_bookmaker]['markets'][0]['outcomes'])
+                best_odds = [[None, None, float('-inf')] for _ in range(num_outcomes)]
                 for outcome in range(num_outcomes):
-                    bookmaker_odds = round(float(event[count_bookmaker]['markets'][0]['outcomes'][outcome]['price']), 2) # prize round to 2 decimals
-                    odd_name = event[count_bookmaker]['markets'][0]['outcomes'][outcome]['name'] # Name of team of the current quota
-                    event_outcomes.append([event[count_bookmaker]['title'], odd_name, bookmaker_odds])
-                    # print(bookmaker[1]['title'])
-                    # if bookmaker_odds > current_best_odds:
-                    #     best_odds[outcome][0] = bookmaker['titles']
-        
-        print(event_outcomes)
+                    bookmaker_odds = round(float(event[count_bookmaker]['markets'][0]['outcomes'][outcome]['price']), 2) # price round to 2 decimals
+                    current_best_odd = best_odds[outcome][2]
 
-# TODO: find algorithm that splits the odds, find the best and returns a list like this: [bookmaker, name, price]
+                    if bookmaker_odds > current_best_odd:
+                        best_odds[outcome][0] = event[count_bookmaker]['title']
+                        best_odds[outcome][1] = event[count_bookmaker]['markets'][0]['outcomes'][outcome]['name']
+                        best_odds[outcome][2] = bookmaker_odds
+            all_best_odds.append(best_odds)
+        return all_best_odds
+
+    def calculate_arbitrage_bets(self):
+        pass
 
 
 def main():
     x = Event()
-    x.parse_df()
+    x.list_of_best_odds()
 
 if __name__ == '__main__':
     main()
